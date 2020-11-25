648c24c88b373498392f43f231a7672e36d7e293
Bartek <bartosz.fluder@gmail.com>
2020-11-25 15:43:54 +0100

    Jinja template modified. Backend logic mofified. Both can adapt to game mode. Upload files working properly.

--- server_memo/__init__.py
+++ server_memo/__init__.py
@@ -6,11 +6,11 @@ def create_app():
     app = Flask(__name__)
 
     script_path = pathlib.Path(__file__).parent.absolute()
-    img_path = pathlib.Path(script_path, "static/imgs")
     down_path = pathlib.Path(script_path, "static/imgs/User")
+    back_down_path = pathlib.Path(script_path, "static/imgs/User/photos")
 
-    app.config['IMG_COL'] = img_path
-    app.config['USER_IMG'] = down_path
+    app.config['USER_FRONT'] = down_path
+    app.config['USER_BACK'] = back_down_path
     app.config['AV_EXTENSIONS'] = ["JPEG", "JPG", "PNG"]
     app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
--- server_memo/main_view.py
+++ server_memo/main_view.py
@@ -12,10 +12,12 @@ def main_view():
 
     return render_template('main_view.html')
 
+
 @memo_game.route('/choose_own')
 def create_uploads():
 
     return render_template('upload.html')
+
 
 @memo_game.route("/upload-image", methods=["GET", "POST"])
 def upload_image():
@@ -24,10 +26,15 @@ def upload_image():
 
         if 'files[]' not in request.files:
             print('no files detected')
-            return redirect(request.url)
+            return redirect(url_for('memo.create_uploads'))
+
+        if 'back' not in request.files:
+            return redirect(url_for('memo.create_uploads'))
 
         files = request.files.getlist('files[]')
+        file_back = request.files.get('back')
         print(files)
+        print(file_back)
 
         for file in files:
             if file.filename == "":  # esuring that file has a file name.
@@ -37,12 +44,23 @@ def upload_image():
             if allowed_image_filename(file.filename):
                 filename = secure_filename(file.filename)
 
-                file.save(os.path.join(current_app.config['USER_IMG'], filename))
+                file.save(os.path.join(current_app.config['USER_FRONT'], filename))
                 print('image saved')
 
             else:
                 flash('That file extension is not allowed')
                 redirect(url_for('memo_game.main_view'))
+
+        if file_back.filename == '':
+            print("No filename")
+            return redirect(url_for('memo.main_view'))
+        if allowed_image_filename(file_back.filename):
+            filename = secure_filename(file_back.filename)
+
+            file_back.save(os.path.join(current_app.config['USER_BACK'], filename))
+        else:
+            flash('That file extension is not allowed')
+            redirect(url_for('memo_game.main_view'))
 
     return redirect(url_for('memo.main_view'))  # check if correct syntax
 
@@ -63,12 +81,11 @@ def allowed_image_filename(filename):
         return False
 
 
-@memo_game.route('/game')
-def game():
+def get_imgs(file_path):
+
     imgs = {}.fromkeys(range(1, 13), "")
     possible_places = list(range(1, 13))
-    script_path = pathlib.Path(__file__).parent.absolute()
-    file_path = pathlib.Path(script_path, "static/imgs/Default")
+
     files = os.listdir(file_path)
     files.remove('photos')
     for i in range(1, 7):
@@ -80,7 +97,31 @@ def game():
         place = random.choice(possible_places)
         possible_places.remove(place)
         imgs[place] = file
-    return render_template('template.html', images=imgs)
+    return imgs
+
+
+@memo_game.route('/game/<mode>')
+def game(mode):
+    print(f'mode: {mode}')
+    script_path = pathlib.Path(__file__).parent.absolute()
+    if mode == "default":
+        file_path = pathlib.Path(script_path, "static/imgs/Default")
+        imgs = get_imgs(file_path)
+        cover = 'cover.jpg'
+        return render_template('template.html', images=imgs, folder="Default", cover=cover)
+    elif mode == 'custom':
+        file_path = pathlib.Path(script_path, "static/imgs/User")
+        imgs = get_imgs(file_path)
+        file_path = pathlib.Path(script_path, "static/imgs/User/photos")
+        files = os.listdir(file_path)
+        cover = files[0]
+        return render_template('template.html', images=imgs, folder="User", cover=cover)
+    else:
+        redirect(url_for('memo.main_view'))
+
+
+
+
 
 
 
